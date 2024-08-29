42294fd6d00f787cac4b5c88c9c920126c4bf97a
gauravgunwant <gunwantbhavesh100@gmail.com>
2024-08-29 15:08:50 +0530

    changes made in STL.cpp

--- STL.cpp
+++ STL.cpp
@@ -5,7 +5,8 @@ void PairSTL(){
     pair<int,int> p1= {1,2};
     cout << p1.first << " " << p1.second << endl;
 
-    pair<int,char> p2= {1, 'x'};
+    // another way to make pair. 
+    pair<int,char> p2= make_pair(1, 'x'); 
     cout << p2.first << " " << p2.second << endl;
 }
 
