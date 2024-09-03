5fb9a413a0ae53ad7b2ebf45d3dfd066f27b809c
gauravgunwant <gunwantbhavesh100@gmail.com>
2024-09-03 18:03:59 +0530

    Done all STL begineer DSA part

--- STL.cpp
+++ STL.cpp
@@ -369,8 +369,285 @@ void SetSTL(){
 }
 void MultiSetSTL(){
     // same as set , but it stores repeated elements too in the ascending order.
-    
+    multiset<int> ms;
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(2);
+    ms.insert(25);
+    ms.insert(5);
+    ms.insert(6);
 
+    for (auto i: ms){
+        cout << i << " "; // 1 1 1 1 2 5 6 25 
+    }
+    cout << endl;
+    // almost everything is same but some functions like :-
+
+    ms.erase(1);  // it will erase all the 1's
+    for (auto i: ms){
+        cout << i << " "; // 2 5 6 25 
+    }
+    cout << endl;
+
+    // to erase only 1 --> 1s..
+    multiset<int> ms2;
+    ms2.insert(1);
+    ms2.insert(1);
+    ms2.insert(1);
+    ms2.insert(1);
+    ms2.insert(2);
+    ms2.insert(25);
+    ms2.insert(5);
+    ms2.insert(6);
+    ms2.erase(ms2.find(1));
+    for (auto i: ms2){
+        cout << i << " "; // 1 1 1 2 5 6 25  
+    }
+    cout << endl;
+
+    // also cout() function is too diff.
+
+    cout << ms2.count(1) << endl; // 3 as there are 3, 1's   
+    
+    //other then this all are same functions wrt sets.
+}
+void UnorderedSetSTL(){
+    // in this there is no organised set of data, but
+    // it will keep only unique data , i.e., no
+    // repeated data will be kept.
+    // every function is same as set but it won't be able
+    // to perform upper and lower bound as they are not
+    // sorted. 
+    // this set's time complixity is O(1) but in very rare cases
+    // when data collides or big amount of data then,
+    // Time complexity will be O(N), other then, it
+    // is faster.. 
+    // we use sets when we want arranged data..
+    // we use this for searching the data.. 
+
+    unordered_set<int> uos;
+    uos.insert(1);
+    uos.insert(2);
+    uos.insert(1);
+    uos.insert(4);
+    uos.insert(11);
+    uos.insert(2);
+    uos.insert(3);
+    uos.insert(33);
+
+    for (auto i: uos){
+        cout << i << " "; // 3 4 2 33 11 1 (unordered data)
+    }
+    cout << endl;
+    // searching data:- a
+    auto x = uos.find(7);
+    if (x != uos.end()){
+        cout << *x; 
+    }else{
+        cout << "Element not found" << endl ;
+    }
+
+}
+void MapSTL(){
+    // maps contains key and value.!
+    map<int, string> mpp;
+    mpp[1] = "Bhavesh";
+    mpp[2] = "Gaurav";
+    mpp[2] = "GauraVV";
+    mpp[3] = "Sample";
+    mpp.insert({4, "Hitman"});
+
+    // to print
+    for (auto i : mpp){
+        cout << i.first << " => " << i.second << endl; 
+    }
+
+    // size
+    cout << mpp.size() << endl;
+
+    // to find
+    auto it = mpp.find(5);
+    if (it != mpp.end()){
+        cout << (*it).first << " " << (*it).second << endl;
+    }else {
+        cout << "Invalid" << endl;
+    }
+
+    // lowwer nd upperbound.
+    auto ix = mpp.lower_bound(4);
+    cout << (*ix).first << " " << (*ix).second << endl;
+
+}
+void UnorderedMapSTL(){
+    // almost same as  unordered set.. but unique
+    unordered_map<int, string> mpp;
+    mpp[1] = "Bhavesh";
+    mpp[5] = "Gaurav";
+    mpp[2] = "GauraVV";
+    mpp[3] = "Sample";
+    mpp.insert({4, "Hitman"});
+    for (auto i : mpp){
+        cout << i.first << " => " << i.second << endl; 
+    }
+    // no lower bound and upper bound, for fast searching
+}
+void MultiMapSTL(){
+    // this will sort all the keys (similar too) in 
+    // ascending order. assign value and keys
+    // using insert function
+    multimap<int, string> mpp;
+    mpp.insert({1,"One"});
+    mpp.insert({1,"OneE"});
+    mpp.insert({1,"OOne"});
+    mpp.insert({2,"Two"});
+    mpp.insert({2,"TwoO"});
+    mpp.insert({3,"Three"});
+    mpp.insert({0,"zero"});
+    for (auto i: mpp){
+        cout << i.first << "=>" << i.second << endl;
+    }
+    cout << endl;
+    // now to find the range where 2 is.. 
+    // we will use equal_range() function
+    // it returns pair of iterators, first one returns
+    // strting range, second returns ending range
+    auto it = mpp.equal_range(2);
+    for (auto i = it.first; i != it.second; i++){ // we use !=
+    // because map doesn't stores in a consecutive order. that's
+    // why we use != instead of <=...
+        cout << (*i).first << "=> " << (*i).second << endl;
+    }
+}
+void SortFunctionSTL(){
+    int arr[5] = {7,4,2,8,0};
+    sort(arr, arr+5); // range of sorting, the lst range given will
+    // not be included so we have to +1 of the range ..
+    for (int i =0 ; i<5; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // similarly for vector or any data type..
+
+    vector<int> vect ={7,4,2,8,0};
+    sort(vect.begin(),vect.end()); // end() is after the end element.
+    for (auto i: vect){
+        cout << i << " ";
+    }
+    cout << endl;
+
+}
+void AccumulateFunctionSTL(){
+    // it takes 3 argument, starting and ending range..
+    // with the integer we want to get summision of 
+    // array that will be added.
+    int arr[5] = {7,4,2,8,0};
+    cout << accumulate(arr,arr+5,1) << endl;
+}
+void CountFunctionSTL(){
+    int arr[6]= {1,5,1,6,8,2};
+    // it counts how many times the element is repeating
+    // it takes range and the number which has to be counted.
+    cout << count(arr,arr+6,1);
+    cout << endl;
+} 
+void FindFunctionSTL(){
+    // find is a function which is returns iterator.
+    // it takes 3 arg, range and element to be finded out.
+    int arr[5] = {1,4,3,6,8};
+    auto it= find(arr,arr+5,0);
+    if (it == (arr +5) ) {
+        cout << "not found";
+    }else{
+        cout << *it << endl; 
+    }
+}   
+void PermutationSTL(){
+    // it works in string ,array or any other datatype.
+    // so, there are two types of permutation :- previous and next permutation .. these permutation works in order. like if there is "ABC" then it has 6 permutations , but if we enter "cab" then it would only have 2 permutation as output..
+    // lets see next permutation first. we have two arguements , that is ranges
+    string x= "abc";
+    do {
+        cout << x << endl;
+    }while (next_permutation(x.begin(),x.end()));  // abc to cba
+    cout << endl;
+
+    do {
+        cout << x << endl;
+    }while (next_permutation(x.begin(),x.begin()+2)); // only ab will be taken, no c will be taken.
+    cout << endl;
+
+    // it works in correct order!
+    string x2= "cab"; // next possible order will be printed
+    do {
+        cout << x2 << endl;
+    }while (next_permutation(x2.begin(),x2.end())); 
+    cout << endl;
+
+    // Similarly there is previous permutation..! it will give output in reverse order 
+    cout << "Prev Permutation" << endl;
+    string y = "cba";
+    do {
+        cout << y << endl; // cba to abc.
+    }while (prev_permutation(y.begin(),y.end())); 
+    cout << endl;
+    // similarly here also we will get permutations in ordered manner.!
+}
+void MaxMinElementSTL(){
+    // it also takes args as range but it works using iterator.
+    int arr[5] = {10,8,3,5,8};
+    auto it = max_element(arr,arr+5);
+    cout << *it << endl; // 10
+    auto itr = min_element(arr,arr+5);
+    cout << *itr << endl; // 3
+}
+void ReverseSTL(){
+    // it reverses the container.!
+    int arr[5] = {3,9,1,12,5};
+    reverse(arr,arr+5);
+    for (int i = 0 ; i<5 ; i++){
+        cout << arr[i] << " "; // 5 12 1 9 3
+    }
+}
+void PowerSTL(){
+    // it gives power of any number we want.
+    cout << pow(5,2) << endl; //25 as 5^2 = 25
+}
+bool ComparatorSTLx1(int e1, int e2){
+    if (e1 < e2) {
+        return false;
+    } else {
+        return true;
+    }
+}
+bool ComparatorSTLx2(pair<int,int> el1 ,pair<int,int> el2){
+    if (el1.second < el2.second) return false;
+    if (el1.second > el2.second) return true;
+    if (el1.first < el2.first) return true;
+    return false;
+
+}
+void ComparatorMainSTL(){
+    // when we use sort function , the function makes easier to sort, but what it does is , it has a internal comparator which compares the elements of the container.. like if element1 < element2 then true else false that means if el1 should be before or after el2.! by taking two elements.
+    int arr[6]={5,1,3,7,1,9};
+    sort(arr,arr+6, ComparatorSTLx1); // here we will give our comparator that will work according to our logic as third arguement.! and the logic we will be using will be for descending to ascending order
+    for (int i=0;i<5;i++){
+        cout << arr[i] << " "; // 1 1 3 5 7 
+    }
+    cout << endl;
+
+// similarly another example.! for pair datatype
+    pair<int , int> arr2[] = {{1,3},{5,4},{3,4},{2,3}}; 
+    // now i want to arrange these in such a manner that the first will be in ascending and second will be in descending order. that is         {{3,4},{5,4},{1,3},{2,3}}
+    sort(arr2,arr2+4, ComparatorSTLx2);
+    for (int i=0;i<4;i++){
+        cout << "{" << arr2[i].first << "," << arr2[i].second << "}"; 
+    }
+    cout << endl;
+
+    // comparators are basically our logics that how we want the program to work.
 }
 int main() {
     // PairSTL();
@@ -381,6 +658,20 @@ int main() {
     // QueueSTL();
     // PQueueSTL();
     // SetSTL();
-    MultiSetSTL();
+    // MultiSetSTL();
+    // UnorderedSetSTL();
+    // MapSTL();
+    // UnorderedMapSTL();
+    // MultiMapSTL();
+    // SortFunctionSTL();
+    // AccumulateFunctionSTL();
+    // CountFunctionSTL();
+    // FindFunctionSTL();
+    // PermutationSTL();
+    // MaxMinElementSTL();
+    // ReverseSTL();
+    // PowerSTL();
+    ComparatorMainSTL();
+
     return 0;
 }
